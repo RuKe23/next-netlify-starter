5709b12aaca37ab1f0889320a8e8b8f0b117a6bf
RuKe23 <143714641+RuKe23@users.noreply.github.com>
2024-06-01 22:15:40 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/10556791?color=000000" allowfullscreen="" width=100% height=100%><a href="https://ru08.itch.io/anti-smoking-campaign">Play anti-smoking campaign on itch.io</a></iframe>
+      <body style="margin:0px;padding:0px;overflow:hidden">
+        <iframe src="https://itch.io/embed-upload/10556791?color=101010" allowfullscreen='' frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
+      </body>
     </div>
   )
 }
